be3c4e2439a2078b1ab40605e2e9efbfaf0a2235
emtupp <emilyc.tupper@gmail.com>
2021-01-13 19:53:02 +0000

    add tests for user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :password, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
+  validates :password, confirmation: { case_sensitive: true }
+  validates :name, presence: true
+  validates :email, presence: true
+  validates_uniqueness_of :email, { case_sensitive: false }
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it "saves successfully when given all required fields" do
+      @user = User.new(name: 'User 1', email: 'TEXT@TEXT.COM', password: 'password', password_confirmation: 'password')
+    end
+
+    it "is not valid without a password" do
+      @user = User.new(name: 'User 1', email: 'TEXT@TEXT.COM', password_confirmation: 'password')
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a password confirmation" do
+      @user = User.new(name: 'User 1', email: 'TEXT@TEXT.COM', password: 'password')
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid with wrong password confirmation" do
+      @user = User.new(name: 'User 1', email: 'TEXT@TEXT.COM', password: 'password', password_confirmation: 'wrong')
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid with same password, but in upper case" do
+      @user = User.new(name: 'User 1', email: 'TEXT@TEXT.COM', password: 'password', password_confirmation: 'PASSWORD')
+      expect(@user).to_not be_valid
+    end
+
+    it "requires a password with a minimum length of five" do
+      @user = User.new(name: 'User 1', email: 'TEXT@TEXT.COM', password: 'four', password_confirmation: 'four')
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without an email" do
+      @user = User.new(name: 'User 1', password: 'password', password_confirmation: 'password')
+      expect(@user).to_not be_valid
+    end
+
+    it "prevents two users from having the same email, not case sentsitive" do
+      @user = User.new(name: 'User 1', email: 'TEXT@TEXT.COM', password: 'password', password_confirmation: 'password')
+      @user2 = User.new(name: 'User 2', email: 'text@text.com', password: 'secure', password_confirmation: 'secure')
+      expect(@user2).to_not be_valid
+    end
+
+    it "is not valid without a name" do
+      @user = User.new(email: 'TEXT@TEXT.COM', password: 'password', password_confirmation: 'password')
+      expect(@user).to_not be_valid
+    end
+
+  end
+
 end
